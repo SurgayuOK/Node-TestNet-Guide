b100d5090b399ab295de8dcad26f48403f14ff11
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-09 21:24:20 +0700

    Update AutoSnapshot.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot.sh
@@ -3,20 +3,15 @@ sudo apt update && sudo apt upgrade -y && sudo apt install zip unzip
 
 # Download Snapshots Inery
 cd ~
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1H1nCpQXjdtapLFjB0SAaVe3YuQhVuu8n' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1H1nCpQXjdtapLFjB0SAaVe3YuQhVuu8n" -O blockchain.zip && rm -rf /tmp/cookies.txt
-
-# Exstraks file
-unzip $HOME/blockchain.zip
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1VgisZqv2lxm6VV_YAUUQ92nMCXIrRo5y' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VgisZqv2lxm6VV_YAUUQ92nMCXIrRo5y" -O blockchain.zip && rm -rf /tmp/cookies.txt  && unzip blockchain.zip  && rm -rf $HOME/blockchain.zip
 
 # Stop Inery Node
 cd $HOME/inery-node/inery.setup/master.node/ && ./stop.sh
 rm -rf $HOME/inery-node/inery.setup/master.node/blockchain
 
 # Masukkan folder snapshots yang baru ke Inery
-cd ~ && rm -rf $HOME/blockchain/ined.pid
-cp -r $HOME/blockchain $HOME/inery-node/inery.setup/master.node/blockchain
+mv $HOME/blockchain $HOME/inery-node/inery.setup/master.node/blockchain
 source ~/.bashrc && which nodine || source ~/.bash_profile
-rm -rf $HOME/blockchain.zip && rm -rf $HOME/blockchain
 
 # Restart Inery Node
 cd $HOME/inery-node/inery.setup/master.node/ && ./start.sh
