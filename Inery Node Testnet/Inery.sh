720a09854fcf26e55b022ab93e98b4c4032f58f4
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-09 23:01:28 +0700

    Update Inery.sh

--- Inery Node Testnet/Inery.sh
+++ Inery Node Testnet/Inery.sh
@@ -262,14 +262,7 @@ break;;
 
 "Snapshot") # snapshot
 clear
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1VgisZqv2lxm6VV_YAUUQ92nMCXIrRo5y' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VgisZqv2lxm6VV_YAUUQ92nMCXIrRo5y" -O blockchain.zip && rm -rf /tmp/cookies.txt  && unzip blockchain.zip  && rm -rf $HOME/blockchain.zip
-cd $HOME/inery-node/inery.setup/master.node/
-./stop.sh
-cd ~ && rm -rf $HOME/inery-node/inery.setup/master.node/blockchain
-mv $HOME/blockchain $HOME/inery-node/inery.setup/master.node/blockchain
-source ~/.bashrc && which nodine || source ~/.bash_profile
-cd $HOME/inery-node/inery.setup/master.node/
-./start.sh
+wget -O $HOME/AutoSnapshot.sh https://raw.githubusercontent.com/SaujanaOK/Node-TestNet-Guide/main/Inery%20Node%20Testnet/Snapshots/AutoSnapshot.sh && bash $HOME/AutoSnapshot.sh
 sleep 1
 clear
 break;;
