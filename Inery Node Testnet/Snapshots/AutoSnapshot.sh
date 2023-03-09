db143af6f6bc2f09ed792241e558c4fe83099e8a
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-09 20:48:15 +0700

    Update AutoSnapshot.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y && sudo apt install zip unzip
 
 # Download Snapshots Inery
 cd ~
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1JMs4oCHNMYSZJZHpykLOI80nVY3PAouB' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JMs4oCHNMYSZJZHpykLOI80nVY3PAouB" -O blockchain.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1H1nCpQXjdtapLFjB0SAaVe3YuQhVuu8n' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1H1nCpQXjdtapLFjB0SAaVe3YuQhVuu8n" -O blockchain.zip && rm -rf /tmp/cookies.txt
 
 # Exstraks file
 unzip $HOME/blockchain.zip
