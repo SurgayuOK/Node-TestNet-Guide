ade4722bcb2d377e03bbaae57ac4c239bb0c4dc9
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-09 23:37:25 +0700

    Update Inery.sh

--- Inery Node Testnet/Inery.sh
+++ Inery Node Testnet/Inery.sh
@@ -383,14 +383,14 @@ break;;
 "Info block tertinggi saat ini") # Info block tertinggi saat ini
 clear
 curl -sSL -X POST 'http://tas.blockchain-servers.world:8888/v1/chain/get_info' -H 'Accept: application/json' | jq
-sleep 123
+sleep 45
 clear
 break;;
 
 "Check Info block Local") # Check Info block Local
 clear
 curl -sSL -X POST 'http://localhost:8888/v1/chain/get_info' -H 'Accept: application/json' | jq
-sleep 123
+sleep 45
 clear
 break;;
 
