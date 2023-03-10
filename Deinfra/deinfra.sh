4b427894edd52b2b7bcd7b886659c13289781653
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 12:13:00 +0700

    Update deinfra.sh

--- Deinfra/deinfra.sh
+++ Deinfra/deinfra.sh
@@ -35,6 +35,10 @@ echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 sudo apt update -y && sudo apt install jq -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && sudo apt install docker-compose
 
 echo -e "\e[1m\e[32m2. Pull Docker... \e[0m" && sleep 1
+# Allow port
+sudo allow ufw 44000
+sufo ufw enable
+
 # pull
 docker pull thepowerio/tpnode
 
