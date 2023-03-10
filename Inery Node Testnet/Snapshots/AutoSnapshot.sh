c08d74454bd369054fc83ae3c0f6d9042b662129
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 08:13:53 +0700

    Update AutoSnapshot.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot.sh
@@ -1,15 +1,12 @@
-# Install zip unzip
+# Update System
 sudo apt update
-sleep 2
 
 # Download Snapshots Inery
 wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt && mkdir -p $HOME/blockchain && tar -xvzf blockchain.tar.gz -C blockchain/ && rm blockchain.tar.gz
-sleep 1
 
 # Stop Inery Node
 cd $HOME/inery-node/inery.setup/master.node/
 ./stop.sh
-sleep 2
 
 # Remove File
 rm -rf $HOME/inery-node/inery.setup/master.node/blockchain
