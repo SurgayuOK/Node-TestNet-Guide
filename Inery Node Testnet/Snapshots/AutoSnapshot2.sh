20227b420669e4fdfcf490812b58cd97af3776bf
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 14:43:48 +0700

    Update AutoSnapshot2.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot2.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot2.sh
@@ -10,12 +10,19 @@ cd $HOME/inery-node/inery.setup/master.node
 cd $HOME/inery-node/inery.setup/master.node
 tar -czvf $HOME/blockchain_backup.tar.gz -c blockchain
 
+# Removing
+cd $HOME/inery-node/inery.setup/master.node
+rm -rf blockchain
+
 # Download Snapshots Inery
 cd $HOME/inery-node/inery.setup/master.node
-rm -rf $HOME/blockchain
 wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt
 
-# Ekeskusi
+# Buat Folder
+cd $HOME/inery-node/inery.setup/master.node
+mkdir -p blockchain
+
+# Amankan Backup
 cd $HOME/inery-node/inery.setup/master.node
 tar -xvzf blockchain.tar.gz -C blockchain
 source ~/.bashrc && which nodine || source ~/.bash_profile
@@ -32,7 +39,7 @@ rm -rf blockchain.tar.gz
 
 # Remove Installasi
 cd
-rm -rf $HOME/AutoSnapshot2.sh
+rm -rf $HOME/AutoSnapshot.sh
 
 # Kembali ke menu utama
 wget -O $HOME/inery.sh https://raw.githubusercontent.com/SaujanaOK/Node-TestNet-Guide/main/Inery%20Node%20Testnet/Inery.sh && chmod 777 $HOME/inery.sh && bash $HOME/inery.sh
