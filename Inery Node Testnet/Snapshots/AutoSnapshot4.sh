ec3574e1ad93d1b6400d2e14aa90ed397aa3d614
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 16:11:26 +0700

    Update AutoSnapshot4.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot4.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot4.sh
@@ -29,7 +29,7 @@ rm -rf blockchain.tar.gz
 
 # Remove Installasi
 cd
-rm -rf $HOME/AutoSnapshot1.sh
+rm -rf $HOME/AutoSnapshot4.sh
 
 # Kembali ke menu utama
 wget -O $HOME/inery.sh https://raw.githubusercontent.com/SaujanaOK/Node-TestNet-Guide/main/Inery%20Node%20Testnet/Inery.sh && chmod 777 $HOME/inery.sh && bash $HOME/inery.sh
