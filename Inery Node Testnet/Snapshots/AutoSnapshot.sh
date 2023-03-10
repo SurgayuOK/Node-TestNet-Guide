0141668d020b40679d51cb4d3ba488f1aec952c1
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 14:42:27 +0700

    Update AutoSnapshot.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot.sh
@@ -12,15 +12,18 @@ tar -czvf $HOME/blockchain_backup.tar.gz -c blockchain
 
 # Removing
 cd $HOME/inery-node/inery.setup/master.node
-rm -rf $HOME/blockchain
+rm -rf blockchain
 
 # Download Snapshots Inery
 cd $HOME/inery-node/inery.setup/master.node
 wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1QftbFHrgZvRgR0yiRUw8_0nTM2LbYd6g' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QftbFHrgZvRgR0yiRUw8_0nTM2LbYd6g" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt
 
-# Ekeskusi
+# Buat Folder
 cd $HOME/inery-node/inery.setup/master.node
-mkdir -p $HOME/blockchain
+mkdir -p blockchain
+
+# Amankan Backup
+cd $HOME/inery-node/inery.setup/master.node
 tar -xvzf blockchain.tar.gz -C blockchain
 source ~/.bashrc && which nodine || source ~/.bash_profile
 sleep 1
