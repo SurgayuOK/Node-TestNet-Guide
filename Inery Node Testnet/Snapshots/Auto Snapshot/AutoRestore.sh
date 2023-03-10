72da3ce2ac7f67d4014568396b85fc74a0e41f2b
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 18:19:08 +0700

    Update AutoRestore.sh

--- Inery Node Testnet/Snapshots/Auto Snapshot/AutoRestore.sh
+++ Inery Node Testnet/Snapshots/Auto Snapshot/AutoRestore.sh
@@ -1,6 +1,41 @@
-cd $HOME/inery-node/inery.setup/master.node && ./stop.sh && rm -rf blockchain.tar.gz && rm -rf blockchain.zip rm -rf && blockchain
-rm -rf $HOME/blockchain && mkdir -p $HOME/inery-node/inery.setup/master.node/blockchain
-tar -xvzf $HOME/blockchain_backup.tar.gz -C $HOME/inery-node/inery.setup/master.node/blockchain
-rm -rf $HOME/blockchain_backup.tar.gz && rm -rf $HOME/blockchain_backup.zip
+clear
+# Task 1 Update
+sudo apt get update
+
+# Task 2
+cd $HOME/inery-node/inery.setup/master.node && ./stop.sh
+
+# Task 3
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain.tar.gz
+
+# Task 4
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain.zip
+
+# Task 5
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain && rm -rf $HOME/blockchain
+
+# Task 6
+cd $HOME/inery-node/inery.setup/master.node && mkdir -p blockchain
+
+# Task 7
+cd $HOME/inery-node/inery.setup/master.node
+tar -xvzf $HOME/blockchain_backup.tar.gz -C blockchain
+
+# Task 8
+cd
+rm -rf $HOME/blockchain_backup.tar.gz
+
+# Task 9
+cd
+rm -rf $HOME/blockchain_backup.zip
+
+# Task 10
 cd $HOME/inery-node/inery.setup/master.node && ./hard_replay.sh
-cd $HOME/inery-node/inery.setup/master.node && ./start.sh && tail -f blockchain/nodine.log
+
+# Task 11
+cd $HOME/inery-node/inery.setup/master.node && ./start.sh
+
+# Task 12
+cd $HOME/inery-node/inery.setup/master.node && tail -f blockchain/nodine.log
+
+# End
