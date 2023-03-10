2bde1c982d5236aada5ab8c2d1e275eee0d7139c
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 16:00:57 +0700

    Update AutoSnapshot.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot.sh
@@ -6,21 +6,11 @@ sudo apt update && sudo apt install zip unzip && sudo apt-get install tar
 cd $HOME/inery-node/inery.setup/master.node
 ./stop.sh
 
-# Removing
-cd $HOME/inery-node/inery.setup/master.node
-rm -rf blockchain
-
 # Download Snapshots Inery
 cd $HOME/inery-node/inery.setup/master.node
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1QftbFHrgZvRgR0yiRUw8_0nTM2LbYd6g' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QftbFHrgZvRgR0yiRUw8_0nTM2LbYd6g" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt
-
-# Buat Folder
-cd $HOME/inery-node/inery.setup/master.node
-mkdir -p blockchain
-
-# Amankan Backup
-cd $HOME/inery-node/inery.setup/master.node
-tar -xvzf blockchain.tar.gz -C blockchain
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain && mkdir -p blockchain
+cd $HOME/inery-node/inery.setup/master.node && tar -xvzf blockchain.tar.gz -C blockchain && rm -rf blockchain.tar.gz
 source ~/.bashrc && which nodine || source ~/.bash_profile
 sleep 1
 
