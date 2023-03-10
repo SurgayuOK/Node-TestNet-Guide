25d2f512fcf4045cac7e9bec19d8d0a3bfab5236
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-10 16:02:36 +0700

    Update AutoSnapshot2.sh

--- Inery Node Testnet/Snapshots/AutoSnapshot2.sh
+++ Inery Node Testnet/Snapshots/AutoSnapshot2.sh
@@ -10,21 +10,11 @@ cd $HOME/inery-node/inery.setup/master.node
 cd $HOME/inery-node/inery.setup/master.node
 tar -czvf $HOME/blockchain_backup.tar.gz -c blockchain
 
-# Removing
-cd $HOME/inery-node/inery.setup/master.node
-rm -rf blockchain
-
 # Download Snapshots Inery
 cd $HOME/inery-node/inery.setup/master.node
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VvgvVtI1iJa81-bYIIhPDGQiwBwrocSp" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt
-
-# Buat Folder
-cd $HOME/inery-node/inery.setup/master.node
-mkdir -p blockchain
-
-# Amankan Backup
-cd $HOME/inery-node/inery.setup/master.node
-tar -xvzf blockchain.tar.gz -C blockchain
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1QftbFHrgZvRgR0yiRUw8_0nTM2LbYd6g' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QftbFHrgZvRgR0yiRUw8_0nTM2LbYd6g" -O blockchain.tar.gz && rm -rf /tmp/cookies.txt
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain && mkdir -p blockchain
+cd $HOME/inery-node/inery.setup/master.node && tar -xvzf blockchain.tar.gz -C blockchain && rm -rf blockchain.tar.gz
 source ~/.bashrc && which nodine || source ~/.bash_profile
 sleep 1
 
@@ -39,7 +29,7 @@ rm -rf blockchain.tar.gz
 
 # Remove Installasi
 cd
-rm -rf $HOME/AutoSnapshot.sh
+rm -rf $HOME/AutoSnapshot2.sh
 
 # Kembali ke menu utama
 wget -O $HOME/inery.sh https://raw.githubusercontent.com/SaujanaOK/Node-TestNet-Guide/main/Inery%20Node%20Testnet/Inery.sh && chmod 777 $HOME/inery.sh && bash $HOME/inery.sh
