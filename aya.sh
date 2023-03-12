63b66a3a0bbbd19b1e3fbc412298444364749cc3
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-12 13:42:27 +0700

    Update aya.sh

--- aya.sh
+++ aya.sh
@@ -16,34 +16,32 @@ sleep 2
 sudo apt-get update && sudo apt-get install tar
 
 # Stop Node
-cd $HOME/inery-node/inery.setup/master.node; ./stop.sh
+cd $HOME/inery-node/inery.setup/master.node && ./stop.sh
 
 # Backup data state
-cd $HOME/inery-node/inery.setup/master.node/blockchain/data && cp -r state $HOME/inery-node/state
+cd $HOME/inery-node/inery.setup/master.node && cp -r blockchain/data/state $HOME/inery-node/state
 
 # Backup data reversible
-cd $HOME/inery-node/inery.setup/master.node/blockchain/data/blockchain/blocks && cp -r reversible $HOME/inery-node/reversible
+cd $HOME/inery-node/inery.setup/master.node && cp -r blockchain/data/blockchain/blocks/reversible $HOME/inery-node/reversible
 
 # Delete the directory
-cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain; mkdir -p blockchain
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain
 
 # Download latest snapshot
-cd $HOME/inery-node/inery.setup/master.node && curl -L https://snap.shot.belajarcrypto.tech/inery/blockchain_latest.tar.lz4 | tar -Ilz4 -xf - -C blockchain; source ~/.bashrc && which nodine || source ~/.bash_profile
-
-# Start Node
-cd $HOME/inery-node/inery.setup/master.node && ; ./start.sh
+cd $HOME/inery-node/inery.setup/master.node && curl -L https://snap.shot.belajarcrypto.tech/inery/blockchain_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/inery-node/inery.setup/master.node; source ~/.bashrc && which nodine || source ~/.bash_profile
 
 # Restore Data Backup
-cd $HOME/inery-node/inery.setup/master.node && ./stop.sh; rm -rf blockchain/data/state; rm -rf blockchain/data/blockchain/blocks/reversible; mkdir blockchain/data/state; mkdir blockchain/data/blockchain/blocks/reversible; mv -i $HOME/inery-node/state blockchain/data/state; mv -i $HOME/inery-node/reversible blockchain/data/blockchain/blocks/reversible; source ~/.bashrc && which nodine || source ~/.bash_profile
-
-# Restart Inery Node
-cd $HOME/inery-node/inery.setup/master.node && ./start.sh
+cd $HOME/inery-node/inery.setup/master.node && rm -rf blockchain/data/state; rm -rf blockchain/data/blockchain/blocks/reversible
+cd $HOME/inery-node/inery.setup/master.node && mkdir blockchain/data/state; mkdir blockchain/data/blockchain/blocks/reversible
+cd $HOME/inery-node/inery.setup/master.node && mv -i $HOME/inery-node/state blockchain/data/state
+cd $HOME/inery-node/inery.setup/master.node && mv -i $HOME/inery-node/reversible blockchain/data/blockchain/blocks/reversible
+cd $HOME/inery-node/inery.setup/master.node && source ~/.bashrc && which nodine || source ~/.bash_profile
 
 # Hapus Installan
 cd
 rm -rf $HOME/aya.sh && rm -rf $HOME/ineysnapshot.sh
 
-# Jalankan Logs
-cd $HOME/inery-node/inery.setup/master.node && ./start.sh; && tail -f blockchain/nodine.log
+# Restart Node
+cd $HOME/inery-node/inery.setup/master.node/ && ./start.sh; tail -f blockchain/nodine.log
 
 # End
