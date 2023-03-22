3b5bd4fdaa7e7cd69962f22bf561709422173df6
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-23 06:13:18 +0700

    Update SSL.sh

--- Deinfra/Deinfra Phase 2/SSL.sh
+++ Deinfra/Deinfra Phase 2/SSL.sh
@@ -2,17 +2,21 @@
 clear
 
 # Sett SSL
+curl https://get.acme.sh | sh -s email=$Your_Email
 source $HOME/.bashrc
-acme.sh --issue --standalone --force -d $Your_Domain_Name
+
+# Sett SSL2
+source $HOME/.bashrc
+acme.sh --issue --standalone --force -d $Your_Hostname
 
 # Konfirmasi SSL
-acme.sh --install-cert -d $Your_Domain_Name \
---cert-file /opt/thepower/db/cert/${Your_Domain_Name}.crt \
---key-file /opt/thepower/db/cert/${Your_Domain_Name}.key \
---ca-file /opt/thepower/db/cert/${Your_Domain_Name}.crt.ca.crt
+acme.sh --install-cert -d $Your_Hostname \
+--cert-file /opt/thepower/db/cert/${Your_Hostname}.crt \
+--key-file /opt/thepower/db/cert/${Your_Hostname}.key \
+--ca-file /opt/thepower/db/cert/${Your_Hostname}.crt.ca.crt
 
 # Konfirmasi SSL Lanjutan
-acme.sh --info -d $Your_Domain_Name
+acme.sh --info -d $Your_Hostname
 
 # Done
 rm -rf $HOME/SSL.sh
