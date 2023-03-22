ac3dafae84927a33ca30a480edb15b49037a028c
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-23 04:28:12 +0700

    Update dp2.sh

--- Deinfra/Deinfra Phase 2/dp2.sh
+++ Deinfra/Deinfra Phase 2/dp2.sh
@@ -158,8 +158,25 @@ services:
     command: --interval 3600 --cleanup
 EOF
 
-# Done
+# Socat
 apt-get install socat
+
+# Get acme
+curl https://get.acme.sh | sh -s email=$Your_Email_Address
+source $HOME/.bashrc
+
+# Sett SSL
+source $HOME/.bashrc
+acme.sh --issue --standalone --force -d $Your_Domain_Name
+
+# Konfirmasi SSL
+acme.sh --install-cert -d $Your_Domain_Name \
+--cert-file /opt/thepower/db/cert/${Your_Domain_Name}.crt \
+--key-file /opt/thepower/db/cert/${Your_Domain_Name}.key \
+--ca-file /opt/thepower/db/cert/${Your_Domain_Name}.crt.ca.crt
+acme.sh --info -d $Your_Domain_Name
+
+Done
 rm -rf $HOME/dp2.sh 
 
 # End
