8851f7bf39a3522cd350ad4ad3a328585fd44da0
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-03-23 05:30:15 +0700

    Update dp2.sh

--- Deinfra/Deinfra Phase 2/dp2.sh
+++ Deinfra/Deinfra Phase 2/dp2.sh
@@ -46,11 +46,11 @@ if [ ! $upstream_Link_3 ]; then
 fi
 
 echo ""
-echo -e "Your Domain Name                      : \e[1m\e[35m$Your_Domain_Name\e[0m"
-echo -e "Your Email Address                    : \e[1m\e[35m$YOUR_EMAIL\e[0m"
-echo -e "Your upstream Link 1                  : \e[1m\e[35m$upstream_Link_1\e[0m"
-echo -e "Your upstream Link 2                  : \e[1m\e[35m$upstream_Link_3\e[0m"
-echo -e "Your upstream Link 3                  : \e[1m\e[35m$upstream_Link_3\e[0m"
+echo -e "Your Domain Name               : \e[1m\e[35m$Your_Domain_Name\e[0m"
+echo -e "Your Email Address             : \e[1m\e[35m$YOUR_EMAIL\e[0m"
+echo -e "Your upstream Link 1           : \e[1m\e[35m$upstream_Link_1\e[0m"
+echo -e "Your upstream Link 2           : \e[1m\e[35m$upstream_Link_3\e[0m"
+echo -e "Your upstream Link 3           : \e[1m\e[35m$upstream_Link_3\e[0m"
 echo ""
 
 echo "export Your_Domain_Name=${Your_Domain_Name}" >> $HOME/.bash_profile
